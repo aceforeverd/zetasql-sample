b7a6d9fd93f44b7b6e1d039fcd3ece6879762945
qiliguo <qiliguo@4paradigm.com>
2021-04-19 03:40:46 +0000

    save changes

--- install_zetasql.sh
+++ install_zetasql.sh
@@ -5,8 +5,8 @@ set -eE
 cd "$(dirname "$0")"
 export ROOT=$(realpath .)
 
-rm -rf tmp-lib include lib libzetasql.mri
-mkdir -p tmp-lib include lib
+rm -rf tmp-lib include lib libzetasql.mri lib-external libexternal.mri
+mkdir -p tmp-lib include lib lib-external
 
 install_lib() {
     local file
@@ -16,12 +16,26 @@ install_lib() {
     install -D "$file" "$ROOT/tmp-lib/$libname"
 }
 
+install_gen_include_file() {
+    local file
+    file=$1
+    local outfile
+    outfile=$(echo "$file" | sed -e 's/^.*proto\///')
+    install -D "$file" "$ROOT/include/$outfile"
+}
+export -f install_gen_include_file
+
 export -f install_lib
 
 pushd zetasql/bazel-bin/
 find zetasql -type f -iregex ".*/.*\.\(so\|a\)\$" -exec bash -c 'install_lib $0' {} \;
 
+pushd external
+find . -type f -iregex ".*/.*\.\(so\|a\)\$" -exec install -D {} "$ROOT/lib-external" \;
+popd
+
 find zetasql -type f -iname "*.h" -exec install -D {} $ROOT/include/{} \;
+find zetasql -iregex ".*/_virtual_includes/.*\.h\$" -exec bash -c 'install_gen_include_file $0' {} \;
 popd
 
 pushd zetasql/
@@ -35,3 +49,4 @@ echo -e "save\nend\n" >> libzetasql.mri
 ar -M <libzetasql.mri
 ranlib libzetasql.a
 mv libzetasql.a lib/
+
